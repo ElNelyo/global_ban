19ca4f0b99383fa36e0bba5eeb4fd76fe6c9eb3f
ElNelyo <charly.strn@gmail.com>
2019-04-23 20:00:49 +0200

    fix by ju

--- server.lua
+++ server.lua
@@ -1,45 +1,38 @@
-function checkIdentifierBanned( info,type)
-  PerformHttpRequest("https://global-ban.family-v.com/bans/search.php?id="..info.."&warning="..Config.WarningLevel, function(err, rText, headers)
-  if(err==200)then
-    return false
-  else
-    return true
-  end
-  end, "GET", "", {["Content-Type"] = "application/json"});
-
-end
-
-AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
-local playerId = source
-deferrals.defer()
-deferrals.update(': Making sure you are not globally banned')
-
-Citizen.Wait(1000)
-
-local identifiers = GetPlayerIdentifiers(playerId)
-
-local steam = checkIdentifierBanned(GetPlayerId('steam', source),"steam")
-Citizen.Wait(1000)
-
-local license = checkIdentifierBanned(GetPlayerId('license', source),"license")
-Citizen.Wait(1000)
-
-local ip = checkIdentifierBanned(GetPlayerId('ip', source),"ip")
-Citizen.Wait(1000)
-
-if steam and license and ip then
-  deferrals.done()
-else
-  deferrals.done("The Ban Hammer has spoken \n You're globally permanently banned from the united federation of FiveM communities")
-end
-end)
-
-function GetPlayerId(type, id)
-  local identifiers = GetPlayerIdentifiers(id)
-  for i = 1, #identifiers do
-    if string.find(identifiers[i], type, 1) ~= nil then
-      return identifiers[i]
+local ban = {}
+function checkIdentifierBanned(identifiers,name, pos)
+  if pos ~= nil then
+    local IDENTIFIER = identifiers[pos]
+    if IDENTIFIER ~= nil then
+      PerformHttpRequest("https://global-ban.family-v.com/bans/search.php?id="..IDENTIFIER.."&warning="..Config.WarningLevel,
+      function(err, rText, headers)
+        if err == 200 then
+          print("Player ["..IDENTIFIER.."] attempt to connect but he is banned.")
+          TriggerEvent('global_ban:banned', true,name)
+        else
+          checkIdentifierBanned(identifiers, pos + 1)
+        end
+        end, "GET", "", {["Content-Type"] = "application/json"});
+      end
     end
   end
-  return false
-end
+
+  AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
+  local playerId = source
+  ban[name] = false
+  deferrals.defer()
+  deferrals.update('Making sure you are not globally banned')
+  local identifiers = GetPlayerIdentifiers(playerId)
+  checkIdentifierBanned(identifiers,name, 1)
+  Citizen.Wait(1000)
+  if not ban[name] then
+    deferrals.done()
+  else
+    deferrals.done("The Ban Hammer has spoken \n You're globally permanently banned from the united federation of FiveM communities")
+  end
+  end)
+
+  RegisterNetEvent('global_ban:banned')
+  AddEventHandler('global_ban:banned', function(bool,name)
+  local playerId = source
+  ban[name] = bool
+  end)
