2d719e1dbeaf2015eb5bacd28fc8010770a8435f
ElNelyo <charly.strn@gmail.com>
2019-04-23 12:02:25 +0200

    4indent 4 ever

--- server.lua
+++ server.lua
@@ -1,33 +1,33 @@
 function checkIdentifierBanned( info,type)
-    PerformHttpRequest("https://global-ban.family-v.com/bans/search.php?id="..info.."&warning="..Config.WarningLevel, function(err, rText, headers)
-            if(err==200)then
-                  return false
-                end
-            end, "GET", "", {["Content-Type"] = "application/json"});
-        return true
+  PerformHttpRequest("https://global-ban.family-v.com/bans/search.php?id="..info.."&warning="..Config.WarningLevel, function(err, rText, headers)
+  if(err==200)then
+    return false
+  end
+  end, "GET", "", {["Content-Type"] = "application/json"});
+  return true
 end
 
 AddEventHandler("playerConnecting", function(name, setCallback, deferrals)
-    local playerId = source
-    deferrals.defer()
-    deferrals.update(': Making sure you are not globally banned')
+local playerId = source
+deferrals.defer()
+deferrals.update(': Making sure you are not globally banned')
 
-    Citizen.Wait(1000)
+Citizen.Wait(1000)
 
-    local identifiers = GetPlayerIdentifiers(playerId)
+local identifiers = GetPlayerIdentifiers(playerId)
 
-    local steam = checkIdentifierBanned(identifiers[1],"steam")
-    Citizen.Wait(1000)
+local steam = checkIdentifierBanned(identifiers[1],"steam")
+Citizen.Wait(1000)
 
-    local steam = checkIdentifierBanned(identifiers[2],"license")
-    Citizen.Wait(1000)
+local steam = checkIdentifierBanned(identifiers[2],"license")
+Citizen.Wait(1000)
 
-    local steam = checkIdentifierBanned(identifiers[3],"ip")
-    Citizen.Wait(1000)
+local steam = checkIdentifierBanned(identifiers[3],"ip")
+Citizen.Wait(1000)
 
-    if steam and license and ip then
-      deferrals.done()
-    else
-      deferrals.done("The Ban Hammer has spoken \n You're globally permanently banned from the united federation of FiveM communities")
-    end
-    end)
+if steam and license and ip then
+  deferrals.done()
+else
+  deferrals.done("The Ban Hammer has spoken \n You're globally permanently banned from the united federation of FiveM communities")
+end
+end)
